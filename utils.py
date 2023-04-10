5e0f08f83277e4c10446420cb0c632ae3c5b2624
Faisal Jamil <faisal.jamil@mail.utoronto.ca>
2023-04-09 23:37:40 -0400

    - Finalize training plot generation - Migrated reward_to_score function - Combined all 3 plots into one - Added legend and x and y labels

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import os
+
+import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+from scipy.stats import pearsonr
 
 
 def convert_frames_to_score(row):
@@ -12,18 +14,67 @@ def convert_frames_to_score(row):
         return 1
     else:
         return (frame // 42) + 1
-def plot_training_graph(csv_file, save_name, frames_to_score=False):
-    df = pd.read_csv(csv_file)
+
+
+def convert_reward_to_score(df):
+    previous = 0
+    living_factor = 3.8999999999999986 / 48
+    scores_list = []
+    for i in range(len(df["iteration"])):
+        iteration = df["iteration"][i]
+        totalr = df["total_reward"][i]
+        total_living_iter = iteration - previous
+        score = totalr - living_factor * total_living_iter
+
+        scores_list.append(int(score))
+        previous = iteration
+
+        # Debugging
+        # if score != 0:
+        #     print(score, iteration)
+    return scores_list
+
+
+def plot_training_graph_and_pearson_corr(dqn_file, dsn_file, ppo_file, save_name,
+                                         frames_to_score=False,
+                                         rewards_to_score=False):
+    dqn_df = pd.read_csv(dqn_file)
+    dsn_df = pd.read_csv(dsn_file)
+    ppo_df = pd.read_csv(ppo_file)
     if frames_to_score:
-        df["score"] = df.apply(lambda row: convert_frames_to_score(row), axis=1)
-    plt.scatter(df["iteration"], df["score"])
-    plt.plot(np.unique(df["iteration"]), np.poly1d(np.polyfit(df["iteration"], df["score"], 1))(np.unique(df["iteration"])))
-    # sns.regplot(x=df["iteration"], y=df["score"])
-    # plt.show()
+        ppo_df["score"] = ppo_df.apply(lambda row: convert_frames_to_score(row), axis=1)
+    if rewards_to_score:
+        dqn_df["score"] = convert_reward_to_score(dqn_df)
+        dsn_df["score"] = convert_reward_to_score(dsn_df)
+
+    # PPO graph
+    plt.scatter(ppo_df["iteration"], ppo_df["score"], label="PPO", c="green")
+    # plt.plot(np.unique(ppo_df["iteration"]),
+    #          np.poly1d(np.polyfit(ppo_df["iteration"], ppo_df["score"], 1))(np.unique(ppo_df["iteration"])),
+    #          c="xkcd:light green"
+    #          )
+    print("PPO Pearson Correlation: ", pearsonr(ppo_df["iteration"], ppo_df["score"]))
+
+    # DQN graph
+    plt.scatter(dqn_df["iteration"], dqn_df["score"], label="DQN", c="orangered")
+    print("DQN Pearson Correlation: ", pearsonr(dqn_df["iteration"], dqn_df["score"]))
+
+    # DSN graph
+    plt.scatter(dsn_df["iteration"], dsn_df["score"], label="DSN", c="blue")
+    print("DSN Pearson Correlation: ", pearsonr(dsn_df["iteration"], dsn_df["score"]))
+
+    plt.xlabel("Training Iteration")
+    plt.ylabel("Score")
+    plt.legend(loc="upper left")
+    fig = matplotlib.pyplot.gcf()
+    fig.set_size_inches(10, 7)
     plt.savefig(save_name)
 
 
 if __name__ == "__main__":
-    plot_training_graph(os.path.join("PPO", "pm_ppo_final_version_with_8_instances", "output.csv"),
-                        "pm_ppo_final_version_with_8_instances_training.png",
-                        True)
+    dqn_path = os.path.join("CSV Files for Report", "output_dqn.csv")
+    deep_sarsa_path = os.path.join("CSV Files for Report", "output_dsn.csv")
+    ppo_path = os.path.join("CSV Files for Report", "output_ppo.csv")
+    save_file_name = "training_vs_score_graph.png"
+
+    plot_training_graph_and_pearson_corr(dqn_path, deep_sarsa_path, ppo_path, save_file_name, True, True)
